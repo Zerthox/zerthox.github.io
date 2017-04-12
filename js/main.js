39a6c868070c86d2818a833915f859ac27552a24
Zerthox <zerthox@gmail.com>
2017-04-12 17:46:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
     $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        crossDomain: true,
+        beforeSend: setHeader,
         url: "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=01A9EC52293BBAC648A4210DAFC85B7F&steamid=76561198088657844&format=json",
         success: function(result) {
-            console.log(JSON.parse(result));
+            g = JSON.parse(result).response.games;
+            for (i = 0; i < g; i++) {
+            	if (g[i].appid === "730") {
+            		$(".csgo .ghost-text").text((g[i].playtime_forever/60) + "h");
+            	}
+            	else if (g[i].appid === "386360") {
+            		$(".smite .ghost-text").text((g[i].playtime_forever/60) + "h");
+            	} 
+            	else if (g[i].appid === "365590") {
+            		$(".division .ghost-text").text((g[i].playtime_forever/60) + "h");
+            	}
+            }
         }
     });
 });
