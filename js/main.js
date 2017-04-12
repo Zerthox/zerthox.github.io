800200f808e5fce1d54a151f9b13c0d4b2a61b52
Zerthox <zerthox@gmail.com>
2017-04-12 21:02:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             }
         });
         $.ajax({
-            url: https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=01A9EC52293BBAC648A4210DAFC85B7F&steamid=76561198088657844&format=json",
+            url: "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=01A9EC52293BBAC648A4210DAFC85B7F&steamid=76561198088657844&format=json",
             success: function(result) {
                 g = result.response.games;
                 for (i = 0; i < g.length; i++) {
