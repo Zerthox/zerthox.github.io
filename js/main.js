c70975d6d60e7083c7a1b5858e70df77f73b0a28
Zerthox <zerthox@gmail.com>
2017-04-12 18:26:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
     $.ajax({
         url: "https://crossorigin.me/https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=01A9EC52293BBAC648A4210DAFC85B7F&steamid=76561198088657844&format=json",
         success: function(result) {
-            g = JSON.parse(result).response.games;
-            for (i = 0; i < g; i++) {
-            	if (g[i].appid === "730") {
-            		$(".csgo .ghost-text").text((g[i].playtime_forever/60) + "h");
+            g = result.response.games;
+            for (i = 0; i < g.length; i++) {
+            	if (g[i].appid === 730) {
+            		$(".csgo .ghost-text").text(Math.round(g[i].playtime_forever/60) + "h");
             	}
-            	else if (g[i].appid === "386360") {
-            		$(".smite .ghost-text").text((g[i].playtime_forever/60) + "h");
+            	else if (g[i].appid === 386360) {
+            		$(".smite .ghost-text").text(Math.round(g[i].playtime_forever/60) + "h");
             	} 
-            	else if (g[i].appid === "365590") {
-            		$(".division .ghost-text").text((g[i].playtime_forever/60) + "h");
+            	else if (g[i].appid === 365590) {
+            		$(".division .ghost-text").text(Math.round(g[i].playtime_forever/60) + "h");
             	}
             }
         }
