f441534cdb09ea7c3d5fe0b05018fc2eeb654e4c
Zerthox <zerthox@gmail.com>
2017-04-12 21:02:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     if (window.location.pathname === "/info") {
+        $.ajaxPrefilter(function(options) {
+            if (options.crossDomain && jQuery.support.cors) {
+                options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+            }
+        });
         $.ajax({
-            url: "https://crossorigin.me/https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=01A9EC52293BBAC648A4210DAFC85B7F&steamid=76561198088657844&format=json",
+            url: https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=01A9EC52293BBAC648A4210DAFC85B7F&steamid=76561198088657844&format=json",
             success: function(result) {
                 g = result.response.games;
                 for (i = 0; i < g.length; i++) {
