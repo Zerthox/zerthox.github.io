269d6080a3df12908da93e34099dc7ff20ff0819
Zerthox <zerthox@gmail.com>
2017-04-18 14:26:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 $(document).ready(function() {
     if (window.location.pathname === "/info") {
-        $.ajaxPrefilter(function(options) {
+		$.ajaxPrefilter(function(options) {
             if (options.crossDomain && jQuery.support.cors) {
                 options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
             }
         });
         $.ajax({
-            url: "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=01A9EC52293BBAC648A4210DAFC85B7F&steamid=76561198088657844&format=json",
-            success: function(result) {
-                g = result.response.games;
-                for (i = 0; i < g.length; i++) {
-                    if (g[i].appid === 730) {
-                        $(".csgo .ghost-text").text(Math.round(g[i].playtime_forever/60) + 'h');
+            url: "http://steamcommunity.com/id/zerthox/games?xml=1",
+            success: function(r) {
+                $(r).find("appID").each(function() {
+                	if ($(this).text() === "730") {
+                        $(".csgo .ghost-text").text(Math.round($(this).parent().find("hoursOnRecord").text()) + 'h');
                     }
-                    else if (g[i].appid === 386360) {
-                        $(".smite .ghost-text").text(Math.round(g[i].playtime_forever/60) + 'h');
+                    else if ($(this).text() === "386360") {
+                        $(".smite .ghost-text").text(Math.round($(this).parent().find("hoursOnRecord").text()) + 'h');
                     } 
-                    else if (g[i].appid === 365590) {
-                        $(".division .ghost-text").text(Math.round(g[i].playtime_forever/60) + 'h');
+                    else if ($(this).text() === "365590") {
+                        $(".division .ghost-text").text(Math.round($(this).parent().find("hoursOnRecord").text()) + 'h');
                     }
-                }
+                });
             }
         });
     }
