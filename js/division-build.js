bd9309f1d54d827a2fb158cded3fdf0508f00b34
Zerthox <zerthox@gmail.com>
2017-04-20 22:30:45 +0200

    Update division-build.js

--- js/division-build.js
+++ js/division-build.js
@@ -90,5 +90,5 @@ core.showpage = function(page) {
     }
 };
 core.alert = function(title, msg) {
-    $(".main").html("<div class='error'><div class='error-title'>" + title + "</div><div class='error-content'>" + content + "</div></div>");
+    $(".main").html("<div class='error'><div class='error-title'>" + title + "</div><div class='error-content'>" + msg + "</div></div>");
 };
