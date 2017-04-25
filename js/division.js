f3ef98be5dd7256495e2aa85c64d2962a3100496
Zerthox <zerthox@gmail.com>
2017-04-25 18:44:28 +0200

    Update division.js

--- js/division.js
+++ js/division.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var img = ["https://images4.alphacoders.com/773/773361.jpg",
-  "https://images7.alphacoders.com/602/602603.jpg",
+	"https://images7.alphacoders.com/602/602603.jpg",
 	"https://images5.alphacoders.com/676/676538.jpg",
 	"https://images8.alphacoders.com/605/605515.jpg",
 	"https://images4.alphacoders.com/601/601859.jpg",
