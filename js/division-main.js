404d80bb6b288c974c266e2d1d1141e5268b3263
Zerthox <zerthox@gmail.com>
2017-04-25 11:57:56 +0200

    Update division-main.js

--- js/division-main.js
+++ js/division-main.js
@@ -1,29 +1,37 @@
 $(document).ready(function() {
-	$.ajax({
-		url: "https://api.github.com/repos/Zerthox/zerthox.github.io/contents/division/builds",
-		success: function(result) {
-			for (i = 0; i < result.length; i++) {
+     $.ajax({
+          url: "https://api.github.com/repos/Zerthox/zerthox.github.io/contents/division/builds",
+          success: function(result) {
+               for (i = 0; i < result.length; i++) {
                     var n = result[i].name;
-				if (n.slice(-5) === ".json") {
-     				$.ajax({
+                    if (n.slice(-5) === ".json") {
+                         $.ajax({
                          url: "https://raw.githubusercontent.com/Zerthox/zerthox.github.io/master/division/builds/" + n,
                          success: function(result) {
                               var u = this.url.split("/");
                               $(".build-list").append('<a href="build?id=' + u[u.length - 1].slice(0, -5) + '"></a>');
-                         	var r = JSON.parse(result);
+                              var r = JSON.parse(result);
                               var s = '<div class="list-item"><div class="title">' + r.title + '</div><div class="info">';
-                         	if (r.hasOwnProperty("author"))
-                         		s += 'Author: <a href="' + r.link + '" target="_blank"><span class="author">' + r.author + '</span></a>';
-                         	else
-                         		s += 'Author: <i>Unknown</i>';
-                         	if (r.hasOwnProperty("date"))
-                         		s += '<span class="date">Last updated: ' + r.date + '</span>';
-                         	s += '</div></div>';
-                         	$(".build-list > a:last").html(s);
-     					}
-     				});
+                              if (r.hasOwnProperty("author"))
+                                   s += 'Author: <a href="' + r.link + '" target="_blank"><span class="author">' + r.author + '</span></a>';
+                              else
+                                   s += 'Author: <i>Unknown</i>';
+                              if (r.hasOwnProperty("date"))
+                                   s += '<span class="date">Last updated: ' + r.date + '</span>';
+                              s += '</div>'
+                              if (r.hasOwnProperty("tags")) {
+                                   s += '<div class="tags">'
+                                   for (i = 0; i < r.tags.length; i++) {
+                                        s += '<span class="tag">' + r.tags[i] + '</span>';
+                                   }
+                                   s += '</div>';
+                              }
+                              s += '</div>';
+                              $(".build-list > a:last").html(s);
+                              }
+                         });
                     }
-			}
-		}
-	});
+               }
+          }
+     });
 });
