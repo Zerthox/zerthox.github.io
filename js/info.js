38361aeb8c2176290563986ac4140a3d151e634a
Zerthox <zerthox@gmail.com>
2017-05-01 22:36:18 +0200

    Update and rename main.js to info.js

---
+++ js/info.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $.ajaxPrefilter(function(options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });
+    $.ajax({
+        url: "http://steamcommunity.com/id/zerthox/games?xml=1",
+        success: function(r) {
+            $(r).find("appID").each(function() {
+                if ($(this).text() === "730") {
+                    $(".csgo .ghost-text").text(Math.round($(this).parent().find("hoursOnRecord").text()) + 'h');
+                }
+                else if ($(this).text() === "386360") {
+                    $(".smite .ghost-text").text(Math.round($(this).parent().find("hoursOnRecord").text()) + 'h');
+                } 
+                else if ($(this).text() === "365590") {
+                    $(".division .ghost-text").text(Math.round($(this).parent().find("hoursOnRecord").text()) + 'h');
+                }
+            });
+        }
+    });
+});
