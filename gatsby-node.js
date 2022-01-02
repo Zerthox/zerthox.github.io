f0deaaf53a5e96ab11b22febda4115ee71a6bd0f
Zerthox <zerthox@gmail.com>
2022-01-02 19:41:22 +0100

    Read graphql types async

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require("fs");
-
-const types = fs.readFileSync(require.resolve("./src/data/types.gql"), "utf8");
+const {promises: fs} = require("fs");
 
 /** @type {import("gatsby").GatsbyNode["createSchemaCustomization"]} */
-exports.createSchemaCustomization = ({actions}) => {
+exports.createSchemaCustomization = async ({actions}) => {
+    const types = await fs.readFile(require.resolve("./src/data/types.gql"), "utf8");
+
     actions.createTypes(types);
 };
