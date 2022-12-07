c1ee3a9b619eb3554adb5b2167902b217e39c6d5
Zerthox <zerthox@gmail.com>
2022-12-07 17:28:33 +0100

    Fix codeblocks

--- src/components/markdown/markdown.tsx
+++ src/components/markdown/markdown.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {Children} from "react";
 import {MDXProvider} from "@mdx-js/react";
 import {MDXComponents} from "mdx/types";
 import {Language} from "prism-react-renderer";
 import {Anchor} from "./anchor";
 import {Paragraph} from "./paragraph";
 import {Heading, HeadingProps} from "./heading";
-import {Codeblock, CodeblockProps, InlineCode} from "./code";
+import {Codeblock, InlineCode} from "./code";
 import {List} from "../list";
 import {Banner} from "../banner";
 import {CodeTabs} from "../codetabs";
@@ -19,9 +19,12 @@ export const mdxComponents: MDXComponents = {
     h4: (props) => <Heading type={4} {...props as HeadingProps}/>,
     h5: (props) => <Heading type={5} {...props as HeadingProps}/>,
     h6: (props) => <Heading type={6} {...props as HeadingProps}/>,
-    pre: (props) => <div {...props as unknown as React.ComponentProps<"div">}/>,
-    code: (props) => <Codeblock language={props.className?.replace(/^language-/, "") as Language} {...props as CodeblockProps}/>,
-    inlineCode: InlineCode,
+    pre: ({children}) => {
+        const {children: code, className} = (Children.only(children) as React.ReactHTMLElement<HTMLElement>).props;
+        const language = className?.replace(/^language-/, "") as Language;
+        return <Codeblock language={language}>{code as string}</Codeblock>;
+    },
+    code: InlineCode,
     Anchor,
     Paragraph,
     Heading,
